300bb2c08f853dcd2b18b983f78befa6cfb1a04e
Zach Gallman <zach.ecab@gmail.com>
2023-10-02 16:05:43 -0400

    Added text gradient to Projects text

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -10,7 +10,7 @@ export default function Projects()  {
       id="Portfolio"
     >
       <div className="container mx-auto px-4 py-10">
-        <h2 className="text-center text-3xl font-light py-5">
+        <h2 className="text-center text-3xl font-light py-5 text-gradient">
           Projects
         </h2>
         <div className="flex flex-wrap justify-center columns-auto ">
